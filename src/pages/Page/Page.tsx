58da00a487c9499998cde71d3a0d26804646d000
David Opoku <david@cyboticx.com>
2024-06-13 06:28:19 +0000

    fix for eslint

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+// import React from "react";
 import classes from "./Page.module.scss";
 
-type PageProps = {};
+type PageProps = {
+  item: string;
+};
 
-export default function Page(props: PageProps) {
+export default function Page(_: PageProps) {
   return <div className={classes["container"]}></div>;
 }
